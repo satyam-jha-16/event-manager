05acf32adaea293701b117ebe097cd48784ba5b0
Omniflare <namikazesatyam@gmail.com>
2025-05-01 09:59:48 +0530

    minor fixes

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.22.5-alpine3.19
+FROM golang:1.23-alpine
 
 WORKDIR /src/app
 
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/satyam-jha-16/event-manager/config"
 	"github.com/satyam-jha-16/event-manager/db"
 	"github.com/satyam-jha-16/event-manager/handlers"
@@ -13,23 +14,28 @@ import (
 )
 
 func main() {
-	
+
 	envConfig := config.NewEnvConfig()
 	db := db.Init(envConfig, db.DBMigrator)
 	app := fiber.New(fiber.Config{
 		AppName:      "EventLink",
 		ServerHeader: "Fiber",
 	})
+	app.Use(cors.New(cors.Config{
+        AllowOrigins: "http://localhost:8082, http://192.168.29.250:8082", // add all expected origins here
+        AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+        AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+    }))
 
 	eventRepository := repositories.NewEventRepository(db)
 	ticketRepository := repositories.NewTicketRepository(db)
 	authRepository := repositories.NewAuthRepository(db)
 
 	server := app.Group("/api")
-	//services 
+	//services
 	authService := services.NewAuthService(authRepository)
 	handlers.NewAuthHandler(server.Group("/auth"), authService)
-	
+
 	privateRoutes := server.Use(middlewares.AuthProtected(db))
 
 	handlers.NewEventHandler(privateRoutes.Group("/event"), eventRepository)
